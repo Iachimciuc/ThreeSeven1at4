d207e30f8399a2f905cdeaa10a99f1e2ece8ac84
Iachimciuc <42239924+Iachimciuc@users.noreply.github.com>
2018-09-27 12:09:30 +0300

    Update Client.java

--- OneAtFour/src/main/java/SmppApplication/Client/Client.java
+++ OneAtFour/src/main/java/SmppApplication/Client/Client.java
@@ -1,4 +1,24 @@
 package SmppApplication.Client;
 
+import org.jsmpp.bean.BindType;
+import org.jsmpp.bean.NumberingPlanIndicator;
+import org.jsmpp.bean.TypeOfNumber;
+import org.jsmpp.session.BindParameter;
+import org.jsmpp.session.SMPPSession;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        SMPPSession smppSession = new SMPPSession();
+        SMPPSessionSender smppSessionSender = new SMPPSessionSender(smppSession);
+        final BindParameter bindParameter = new BindParameter(BindType.BIND_TRX, "out", "out", "cp", TypeOfNumber.UNKNOWN, NumberingPlanIndicator.UNKNOWN, null);
+        smppSession.connectAndBind("192.168.1.156", 7777, bindParameter);
+        Scanner s = new Scanner(System.in);
+
+        while (s.hasNext()) {
+            smppSessionSender.sendMessage(s.nextLine());
+        }
+    }
 }
